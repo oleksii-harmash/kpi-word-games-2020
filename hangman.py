d930a6bbf27b8f4b890f536d8e8b65fe0cd7d7bb
Alexey Garmash <72203364+AlexiGarmash@users.noreply.github.com>
2020-11-25 23:13:04 +0200

    hw. 4 Added Enum class & changed choosing game mode
    -Added class Mode based on Enum class
        -added attributes and method
    -Fixed mistake with letters_guessed & successful_letter
    -Fixed mistake with 1 extra space in the end in case
    -Name of var avail_lttrs changed to more suitable and correct

--- hangman.py
+++ hangman.py
@@ -2,11 +2,41 @@
 # Name: Alexey Garmash
 # Students group: KM-04
 # Collaborators: -
-# Time spent: 5 h
+# Time spent: 7 h
 
 from random import choice
 from string import ascii_lowercase
+from enum import Enum, auto
 import time
+
+
+class Mode(Enum):
+    '''
+    A class used to represent an Game Mode. (main class ENum)
+
+    A class for choosing a game mode in hangman.
+    Hangman has 2 modes - easy and hard.
+    Easy or with hints is the mode in which you can see the prompt by entering "*".
+    Hard mode has no tips.
+
+    Attributes
+    ----------
+    hard : int
+        attribute that is responsible for hangman game without hits
+    easy : int
+        attribute that is responsible for hangman game with hits
+
+    Methods
+    -------
+    info(None)
+        Prints information about the selected attributs.
+    '''
+    hard = auto()
+    easy = auto()
+
+    def info(self):
+        print("Name - %s, value - %s" % (self.name, self.value),
+              '\n(Easy - game with hints by "*". Hard - game without hints)')
 
 
 def get_words() -> list:
@@ -34,14 +64,15 @@ def get_complexity(secret_word: str, guesses_remaining: int) -> int:
     return len(set(secret_word)) * guesses_remaining
 
 
-def is_word_guessed(secret_word: str, letters_guessed: list) -> bool:
+def is_word_guessed(secret_word: str, successful_letter: list) -> bool:
     '''
     :param secret_word: string, the word the user is guessing;
     :param letters_guessed: list (of letters), which letters have been guessed so far;
-    :return: boolean, True if all the letters of secret_word are in letters_guessed. False otherwise;
+    :return: boolean, True if all the letters of secret_word are in letters_guessed and
+    prints information word for the winner. False otherwise;
     '''
 
-    if set(secret_word) == set(letters_guessed):
+    if set(secret_word) == set(successful_letter):
         print('-' * 20, f'\nGongratulations, you won!\nTotal score: {get_complexity(WORD, guesses)}')
         return True
     else:
@@ -56,7 +87,7 @@ def get_guessed_word(secret_word: str, letters_guessed: list, letter: str) -> st
     :return: string, comprised of letters, underscores (_), and spaces that represents
     which letters in secret_word have been guessed so far;
     '''
-    return ''.join([letter if letter in letters_guessed else '_ ' for letter in secret_word])
+    return ''.join([letter if letter in letters_guessed else '_ ' for letter in secret_word]).strip()
 
 
 def get_available_letters(letters_guessed: list, letter: str) -> str:
@@ -65,10 +96,10 @@ def get_available_letters(letters_guessed: list, letter: str) -> str:
     :return: string (of letters), comprised of letters that represents which
     letters have not yet been guessed;
     '''
-    return ''.join([letter for letter in avail_lttrs if letter not in letters_guessed])
+    return ''.join([letter for letter in available_letters if letter not in letters_guessed])
 
 
-def check_letter(letter: str) -> bool:
+def is_ascii_symbol(letter: str) -> bool:
     '''
     :param letter: input the user is trying to guess;
     :return: boolean, True, if input is letter, otherwise False;
@@ -76,45 +107,48 @@ def check_letter(letter: str) -> bool:
     return letter != '' and letter.lower() in ascii_lowercase
 
 
-def warnings_left(letter: str) -> bool:
+def warnings_left(letter: str, my_word: str) -> bool:
     '''
     Func checks user input for invalid values and repeated letters;
     :param letter: input the user is trying to guess;
     :return: boolean - True and append letter to list available letters, if check_letter() True,
-    boolean - otherwise False and take away warnings;
+    boolean - otherwise False, print information message about
+    repetition of a letter or use of a banned symbol
+    and take away warnings;
     '''
-    global avail_lttrs, letters_guessed, WORD, warnings
+    global available_letters, letters_guessed, warnings
 
-    is_warning = check_letter(letter) and letter in avail_lttrs
+    is_warning = is_ascii_symbol(letter) and letter in available_letters
     if is_warning:
-        if letter in WORD:
+        if letter in my_word:
             word_letters_guessed.append(letter)
         letters_guessed.append(letter)
-        avail_lttrs = get_available_letters(letters_guessed, letter)
+        available_letters = get_available_letters(letters_guessed, letter)
     else:
         warnings -= 1
         if warnings >= 0:
             print( f'Oops! That is not a valid symbol or you already entered that letter. '
-                   f'You have {warnings} warnings left: ', get_guessed_word(WORD, letters_guessed, letter))
+                   f'You have {warnings} warnings left: ', get_guessed_word(my_word, letters_guessed, letter))
     return is_warning
 
 
-def guesses_left(letter: str) -> bool:
+def guesses_left(letter: str, my_word: str) -> bool:
     '''
     :param letter: input the user is trying to guess;
-    :return: boolean - True and calls func get_guesset_word, if letter in word,
-    boolean - otherwise False and take away attempts (guesses);
+    :return: boolean - True, prints information message
+    and calls func get_guessed_word, if letter in word,
+    boolean - otherwise False, information message and take away attempts (guesses);
     '''
-    global WORD, letters_guessed, guesses
+    global letters_guessed, guesses
 
-    is_guessed = letter in WORD
+    is_guessed = letter in my_word
     if is_guessed:
-        print('Good guess:', get_guessed_word(WORD, letters_guessed, letter))
+        print('Good guess:', get_guessed_word(my_word, letters_guessed, letter))
     else:
-        guesses -= 2 if letter in ['a', 'e', 'i', 'o'] else 1
+        guesses -= 2 if letter in VOWELS else 1
         if guesses >= 0:
             print(f'Oops! That letter is not in my word.\n'
-                  f'Please guess a letter: {get_guessed_word(WORD, letters_guessed, letter)}')
+                  f'Please guess a letter: {get_guessed_word(my_word, letters_guessed, letter)}')
     return is_guessed
 
 
@@ -141,14 +175,15 @@ def match_with_gaps(my_word: str, other_word: str) -> bool:
 def show_possible_matches(my_word: str) -> str:
     '''
     :param my_word: string with _ characters, current guess of secret word
-    :return: nothing, but should print out every word in wordlist that matches my_word
+    :return: returns an string of similar words if matches are True,
+             if matches are False - returns information message(string)
              Keep in mind that in hangman when a letter is guessed, all the positions
              at which that letter occurs in the secret word are revealed.
              Therefore, the hidden letter(_ ) cannot be one of the letters in the word
              that has already been revealed.
     '''
-    global show
-    show.clear()
+
+    show = []
 
     for i in WORDLIST:
         if len(i) == len(''.join(my_word.split(' '))) and match_with_gaps(my_word, i):
@@ -187,19 +222,21 @@ def hangman(secret_word: str, game_mode: int) -> str:
 
     Follows the other limitations detailed in the problem write-up.
     '''
-    global warnings, guesses, avail_lttrs, WORD, letters_guessed
+    global warnings, guesses, available_letters, letters_guessed
     if warnings < 0 or guesses < 0:
-        return print('-' * 20, f'\nSorry, you ran out of guesses/warnings. The word was else:\n"{WORD}"')
+        return print('-' * 20, f'\nSorry, you ran out of guesses/warnings. The word was else:\n'
+                               f'"{WORD}"')
 
-    print('-'*20, f'\nYou have {guesses} guesses left.\nAvailable letters: {avail_lttrs}')
-    letter = str(input('Pless guess a letter: ')).lower()
+    print('-' * 20, f'\nYou have {guesses} guesses left.\nAvailable letters: {available_letters}')
+    letter = str(input('Please guess a letter: ')).lower()
 
     time.sleep(1)
     if letter == '*' and game_mode == 2:
         print(f'Possible word matches are: {show_possible_matches(get_guessed_word(WORD, letters_guessed, letter))}')
         return hangman(WORD, game_mode)
 
-    elif not warnings_left(letter) or not guesses_left(letter) or not is_word_guessed(WORD, word_letters_guessed):
+    elif not warnings_left(letter, WORD) or not guesses_left(letter, WORD) \
+            or not is_word_guessed(WORD, word_letters_guessed):
         return hangman(WORD, game_mode)
 
 
@@ -207,11 +244,17 @@ if __name__ == "__main__":
     # get wordlist from txt and create constants (word)
     WORDLIST = get_words()
     WORD = choose_word(WORDLIST)
+    VOWELS = ('a', 'e', 'i', 'o')
 
     #creating variables
+    #warnings - var of warnings in game, guesses - var of guesses in game
     warnings, guesses = 3, 6
-    avail_lttrs = ascii_lowercase
-    letters_guessed, word_letters_guessed, show = [], [], []
+
+    #all letters that haven't been entered yet
+    available_letters = ascii_lowercase
+
+    #lists for words that were attempts and list for words that were seccessful guessed
+    letters_guessed, word_letters_guessed = [], []
 
     # welcome words
     print(f'Welcome to the game Hangman!\nI am thinking of a word that is {len(WORD)} letters long.\nYou have '
@@ -219,4 +262,4 @@ if __name__ == "__main__":
     time.sleep(1)
 
     # second if for choosing game mode (1 - without hints, 2 - with hints)
-    hangman(WORD, 1)
+    hangman(WORD, Mode.easy.value)
